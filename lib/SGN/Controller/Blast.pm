52b7df5a1a83e53e94817864dd14bb4084092f23
Jonathan "Duke" Leto <jonathan@leto.net>
2011-02-02 09:31:18 -0800

    Render a basic blast form

--- lib/SGN/Controller/Blast.pm
+++ lib/SGN/Controller/Blast.pm
@@ -8,6 +8,7 @@ SGN::Controller::Blast - Catalyst controller for pages dealing with features
 
 use Moose;
 use namespace::autoclean;
+use Data::Dumper;
 
 use HTML::FormFu;
 use URI::FromHash 'uri';
@@ -19,7 +20,42 @@ with 'Catalyst::Component::ApplicationAttribute';
 
 sub view :Path('/blast/') Args(0) {
     my ( $self, $c, @args ) = @_;
-    $c->stash->{template} = "/blast/index.mas";
+
+    my $form = $self->_build_form();
+    warn Dumper [ $form ];
+
+    $c->forward_to_mason_view(
+        '/blast/index.mas',
+        form => $form,
+    );
+}
+
+sub _build_form {
+    my ($self) = @_;
+
+    my $form = HTML::FormFu->new(Load(<<EOY));
+      method: POST
+      attributes:
+        name: blast_form
+        id: blast_form
+      elements:
+          - type: Select
+            name: blast_program
+            label: Program
+
+          - type: Text
+          - name: expect_threshold
+          - name: Expect Threshold (e-value)
+
+          - type: Select
+            name: sequence_set
+            label: Sequence Set
+
+          - type: Submit
+            name: submit
+EOY
+
+    return $form;
 }
 
 __PACKAGE__->meta->make_immutable;
