4bd0bfaffc3efb21bb2b9b0c4b41a296d99036f2
Robert Buels <rmb32@cornell.edu>
2011-03-28 19:04:48 -0700

    restrict the biosource crud service to only curators

--- lib/SGN/Controller/Biosource/API.pm
+++ lib/SGN/Controller/Biosource/API.pm
@@ -3,6 +3,14 @@ use Moose;
 BEGIN { extends 'Catalyst::Controller' }
 
 sub api_base : Chained('/') PathPart('api/v1/biosource') CaptureArgs(0) {
+    my ( $self, $c ) = @_;
+
+    $c->user_exists && $c->user->has_role('curator')
+       or $c->throw(
+           is_client_error => 1,
+           http_status     => 403,
+           public_message  => 'Site curator login required',
+         );
 
 }
 
