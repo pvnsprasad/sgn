b8a270ae3391187340ff0775f3d2336d79057c8d
isaak <iyt2@cornell.edu>
2013-06-04 14:55:12 +0200

    read prediction data and impute for missing values..

--- R/gs.r
+++ R/gs.r
@@ -164,12 +164,21 @@ genoData <- read.table(genoFile,
 
 genoData   <- data.matrix(genoData[order(row.names(genoData)), ])
 
-predictionFile <- grep("prediction_population",
+predictionTempFile <- grep("prediction_population",
                        inFiles,
                        ignore.case = TRUE,
                        fixed = FALSE,
                        value = TRUE
                        )
+
+
+predictionFile <- c()
+if (length(predictionTempFile) !=0 )
+  {
+    predictionFile <- scan(predictionTempFile,
+                       what="character"
+                       )
+}
 
 predictionPopGEBVsFile <- grep("prediction_pop_gebvs",
                        outFiles,
@@ -196,6 +205,7 @@ if (length(predictionFile) !=0 )
                       )
   }
 
+
 #add checks for all input data
 #create phenotype and genotype datasets with
 #common stocks only
@@ -218,7 +228,7 @@ genoData<-genoData[(rownames(genoData) %in% rownames(commonObs)), ]
 phenoTrait <- merge(data.frame(phenoTrait), commonObs, by=0, all=FALSE)
 rownames(phenoTrait) <-phenoTrait[,1]
 phenoTrait[, 1] <- NULL
-print(phenoTrait)
+#print(phenoTrait)
 phenoTrait[, 2] <- NULL
 
 #a set of only observation lines with genotype data
@@ -251,9 +261,37 @@ if (sum(is.na(genoDataMatrix)) > 0)
     genoDataMatrix <- data.matrix(genoDataMatrix)
   }
 
+#impute missing data in prediction data
+
+if (length(predictionData) != 0)
+  {
+    predictionData <- data.matrix(predictionData)
+    print('before imputation prediction data')
+    print(predictionData[1:10, 1:4])
+    if (sum(is.na(predictionData)) > 0)
+      {
+        print("sum of geno missing values")
+        print(sum(is.na(predictionData)))
+        predictionData <-kNNImpute(predictionData, 10)
+        predictionData <-as.data.frame(predictionData)
+
+        #extract columns with imputed values
+        predictionData <- subset(predictionData,
+                                 select = grep("^x", names(predictionData))
+                                 )
+
+        #remove prefix 'x.' from imputed columns
+        names(predictionData) <- sub("x.", "", names(predictionData))
+
+        predictionData <- round(predictionData, digits = 0)
+        predictionData <- data.matrix(predictionData)
+      }
+
+}
 
 #change genotype coding to [-1, 0, 1], to use the A.mat )
 genoDataMatrix <- genoDataMatrix - 1
+
 if (length(predictionData) != 0)
   {
     predictionData <- predictionData - 1
